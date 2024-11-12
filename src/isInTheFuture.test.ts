6cfd1d866f70fb975e8750d1ac5ac053cbd4cfef
LuciusL <lucius.lechner@porsche.digital>
2024-11-12 13:47:36 +0100

    .

--- src/isInTheFuture.test.ts
+++ src/isInTheFuture.test.ts
@@ -1,8 +1,10 @@
-import { test, assert } from "vitest";
+import { test, assert, vi } from "vitest";
 import { train } from "./interfaceTrain";
 import { isInTheFuture } from "./isInTheFuture";
 
 test("Checks if train hasn't already left the station", () => {
+    const date = new Date(2024, 0, 21, 0, 0, 0);
+    vi.setSystemTime(date);
     const sbahn: train = {
         departureTime: new Date(5000, 0, 20, 12, 0, 0, 0),
         trainLine: "S4",
@@ -22,9 +24,11 @@ test("Checks if train hasn't already left the station", () => {
     assert.isTrue(isInTheFuture(sbahn));
 });
 
-test("Checks if train has already left the station", () => {
+test("Checks if train has left station by one millisecond", () => {
+    const date = new Date(2024, 0, 19, 23, 59, 59, 999);
+    vi.setSystemTime(date);
     const sbahn: train = {
-        departureTime: new Date(2024, 0, 20, 12, 0, 0, 0),
+        departureTime: new Date(2024, 0, 20, 0, 0, 0, 0),
         trainLine: "S4",
         trainStops: [
             "Kornwestheim Pbf",
@@ -39,5 +43,5 @@ test("Checks if train has already left the station", () => {
         trainFinalStop: "Stuttgart Schwabstr.",
     };
 
-    assert.isFalse(isInTheFuture(sbahn));
+    assert.isTrue(isInTheFuture(sbahn));
 });
