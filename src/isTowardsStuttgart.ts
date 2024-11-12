349893ff4a5c2ba6f04c891c024daea212d5e3c9
LuciusL <lucius.lechner@porsche.digital>
2024-11-12 14:05:19 +0100

    .

--- src/isTowardsStuttgart.ts
+++ src/isTowardsStuttgart.ts
@@ -1,6 +1,5 @@
 import { train } from "./interfaces";
 
 export function isTowardsStuttgart(train: train): boolean {
-    // return train.trainStops.includes("Bietigheim-Bissingen");
     return train.trainStops.includes("Stuttgart Hbf (tief)");
 }
